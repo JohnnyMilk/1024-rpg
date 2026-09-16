83dbb8e8fb9d73ca65de56dc779c5c27af2203a4
Johnny Muscovado <milk.lord@gmail.com>
2026-09-16 11:16:45 +0800

    Strengthen Boss registry for inherited Boss classes

--- rpg-v2-v0-7/refactor/boss-registry.js
+++ rpg-v2-v0-7/refactor/boss-registry.js
@@ -1,9 +1,11 @@
 (()=>{'use strict';
 class V07BossRegistry{
  constructor(){this.types=new Map()}
- register(id,definition){if(!id||!definition)throw new Error('Boss registration requires id and definition');this.types.set(id,definition);return definition}
- create(id,context){const Def=this.types.get(id);if(!Def)throw new Error('Unknown Boss: '+id);return typeof Def==='function'?new Def(context):Object.assign(Object.create(Def),{context})}
+ register(id,BossClass){if(!id||typeof BossClass!=='function')throw new Error('Boss registration requires id and Boss class');if(window.V07BaseBoss&&!(BossClass.prototype instanceof V07BaseBoss)&&BossClass!==V07BaseBoss)throw new Error('Boss must inherit V07BaseBoss: '+id);this.types.set(id,BossClass);return BossClass}
+ create(id,context){const BossClass=this.types.get(id);if(!BossClass)throw new Error('Unknown Boss: '+id);return new BossClass(context)}
  has(id){return this.types.has(id)}
+ get(id){return this.types.get(id)||null}
+ ids(){return[...this.types.keys()]}
 }
 window.V07Bosses=new V07BossRegistry();
 })();
