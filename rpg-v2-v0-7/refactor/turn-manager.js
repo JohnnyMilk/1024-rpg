f007ccce62971021b94925cef8535287822b8192
Johnny Muscovado <milk.lord@gmail.com>
2026-09-16 11:54:32 +0800

    Make runtime state sole turn owner and block invalid swipe progression

--- rpg-v2-v0-7/refactor/turn-manager.js
+++ rpg-v2-v0-7/refactor/turn-manager.js
@@ -1,8 +1,8 @@
 (()=>{'use strict';
 class V07TurnManager{
- constructor({playerPhase,enemyPhase,mapEventPhase,trace=console.log}){this.handlers={playerPhase,enemyPhase,mapEventPhase};this.phase='IDLE';this.turn=0;this.trace=trace}
- async runPlayerAction(action){if(this.phase!=='IDLE')return false;this.turn++;await this.run('PLAYER',()=>this.handlers.playerPhase(action));await this.run('ENEMY',()=>this.handlers.enemyPhase());await this.run('MAP_EVENT',()=>this.handlers.mapEventPhase());this.phase='IDLE';this.trace('[V07 TURN] COMPLETE',{turn:this.turn});return true}
- async run(name,fn){this.phase=name;this.trace('[V07 TURN] '+name,{turn:this.turn});await fn()}
+ constructor({playerPhase,enemyPhase,mapEventPhase,getTurn=()=>0,trace=console.log}){this.handlers={playerPhase,enemyPhase,mapEventPhase};this.phase='IDLE';this.getTurn=getTurn;this.trace=trace}
+ async runPlayerAction(action){if(this.phase!=='IDLE')return false;const playerResult=await this.run('PLAYER',()=>this.handlers.playerPhase(action));if(playerResult===false){this.phase='IDLE';this.trace('[V07 TURN] INVALID',{turn:this.getTurn()});return false}await this.run('ENEMY',()=>this.handlers.enemyPhase());await this.run('MAP_EVENT',()=>this.handlers.mapEventPhase());this.phase='IDLE';this.trace('[V07 TURN] COMPLETE',{turn:this.getTurn()});return playerResult}
+ async run(name,fn){this.phase=name;this.trace('[V07 TURN] '+name,{turn:this.getTurn()});return await fn()}
 }
 window.V07TurnManager=V07TurnManager;
 })();
