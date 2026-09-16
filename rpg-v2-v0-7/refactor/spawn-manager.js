44863b1c92822537778107050e92fdd72104b882
Johnny Muscovado <milk.lord@gmail.com>
2026-09-16 12:56:55 +0800

    Preserve special hero reincarnation eligibility in spawn core

--- rpg-v2-v0-7/refactor/spawn-manager.js
+++ rpg-v2-v0-7/refactor/spawn-manager.js
@@ -2,7 +2,7 @@
 class V07SpawnManager{
  constructor({board,random=Math.random}){this.board=board;this.random=random}
  pickEmpty(filter=null){let cells=this.board.emptyCells();if(filter)cells=cells.filter(([r,c])=>filter(r,c));if(!cells.length)return null;return cells[Math.floor(this.random()*cells.length)]}
- hero({tier=2,special=false,profession=null,...extra}={}){const p=this.pickEmpty();if(!p)return null;return this.board.add({type:'hero',tier,r:p[0],c:p[1],hits:0,special:!!special,profession:special?profession:null,guard:false,revived:false,...extra})}
+ hero({tier=2,special=false,profession=null,reincarnated=false,reincarnateEligible=null,...extra}={}){const p=this.pickEmpty();if(!p)return null;const eligible=reincarnateEligible===null?!!special&&!reincarnated:!!reincarnateEligible;return this.board.add({type:'hero',tier,r:p[0],c:p[1],hits:0,special:!!special,profession:special?profession:null,guard:false,revived:false,reincarnated:!!reincarnated,reincarnateEligible:eligible,...extra})}
  enemy({damage=1,maxHits=1,...extra}={}){const p=this.pickEmpty();if(!p)return null;return this.board.add({type:'enemy',r:p[0],c:p[1],hits:0,maxHits,damage,...extra})}
  boss({maxHits=10,damage=1,topRows=2,...extra}={}){const p=this.pickEmpty((r)=>r<topRows)||this.pickEmpty();if(!p)return null;return this.board.add({type:'boss',r:p[0],c:p[1],hits:0,maxHits,damage,...extra})}
 }
